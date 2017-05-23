668d466391c0b370e5c90a298c05cc331dcbb83a
Vivek Goel <vivekgoel15@gmail.com>
2017-05-23 13:45:35 +0530

    removed design patterns

--- src/com/test/designpatterns/behavioral/observer/custom/Observer.java
+++
@@ -1,7 +0,0 @@
-package com.test.designpatterns.behavioral.observer.custom;
-
-public abstract class Observer {
-	protected Subject subject;
-
-	public abstract void update();
-}
--- src/com/test/designpatterns/behavioral/observer/custom2/Internet.java
+++
@@ -1,8 +0,0 @@
-package com.test.designpatterns.behavioral.observer.custom2;
-
-class Internet implements Observer {
-    @Override
-    public void update(float interest) {
-           System.out.println("Internet: Interest Rate updated, new Rate is: " + interest);
-    }
-}
