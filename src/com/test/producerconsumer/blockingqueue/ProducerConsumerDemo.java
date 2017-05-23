df525973bcd12864ef516a5186d8cb82e4191782
Vivek Goel <vivekgoel15@gmail.com>
2017-05-23 18:11:13 +0530

    formatting

--- src/com/test/producerconsumer/blockingqueue/ProducerConsumerDemo.java
+++ src/com/test/producerconsumer/blockingqueue/ProducerConsumerDemo.java
@@ -4,30 +4,27 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
- * Using Blocking Queue to implement Producer Consumer Pattern
- * Read more: http://javarevisited.blogspot.com/2012/02/producer-consumer-design-pattern-with.html#ixzz4hOwpnNdT
+ * Using Blocking Queue to implement Producer Consumer Pattern Read more:
+ * http://javarevisited.blogspot.com/2012/02/producer-consumer-design-pattern-with.html#ixzz4hOwpnNdT
+ * 
  * @author vigoel
  *
  */
 public class ProducerConsumerDemo {
 
 	private static final int MAX_QUEUE_SIZE = 10;
-	
-    public static void main(String args[]){
-  
-     //Creating shared object
-     BlockingQueue sharedQueue = new LinkedBlockingQueue(MAX_QUEUE_SIZE);
-     int numberOfElements = 50;
 
-     //Creating Producer and Consumer Thread
-     Thread prodThread = new Thread(new Producer(sharedQueue, numberOfElements));
-     Thread consThread = new Thread(new Consumer(sharedQueue));
+	public static void main(String args[]) {
+		// Creating shared object
+		BlockingQueue sharedQueue = new LinkedBlockingQueue(MAX_QUEUE_SIZE);
+		int numberOfElements = 50;
 
-     //Starting producer and Consumer thread
-     prodThread.start();
-     consThread.start();
-    }
- 
+		// Creating Producer and Consumer Thread
+		Thread prodThread = new Thread(new Producer(sharedQueue, numberOfElements));
+		Thread consThread = new Thread(new Consumer(sharedQueue));
+
+		// Starting producer and Consumer thread
+		prodThread.start();
+		consThread.start();
+	}
 }
-
-
